83a9b398fd1b08186be8cdcc336cb8e519574b75
GoldenChrysus <GoldenChrysus@users.noreply.github.com>
2020-01-10 07:33:23 -0500

    Update link logic

--- src/components/Parser/PlayerTable/OverlayInfo.js
+++ src/components/Parser/PlayerTable/OverlayInfo.js
@@ -1,11 +1,32 @@
 import React from "react";
 
+import DonationService from "../../../services/DonationService";
 import VersionService from "../../../services/VersionService";
 
 class OverlayInfo extends React.Component {
 	componentWillMount() {
+		// Hibiya OverlayPlugin: 45.0.2454.85
+		// ACTWS: 53.0.2785.148
+		// ngld OverlayPlugin: 78
+		let browser_data    = navigator.userAgent.match(/Chrome\/[\d.]+/g);
+		let browser_version = "hibiya";
+
+		if (browser_data) {
+			let chrome_version = +browser_data[0].split("/")[1].split(".")[0];
+
+			if (chrome_version >= 65) {
+				browser_version = "ngld";
+			} else if (chrome_version >= 53) {
+				browser_version = "actws";
+			}
+		} else {
+			// Probably in a non-Chrome desktop browser, so we'll call it ngld for consistency
+			browser_version = "ngld";
+		}
+
 		this.setState({
-			changelog: <p>Loading changelog...</p>
+			changelog : <p>Loading changelog...</p>,
+			chrome    : browser_version
 		});
 
 		VersionService.getLatestChangelogs(3)
@@ -29,9 +50,10 @@ class OverlayInfo extends React.Component {
 				<p>Seeking a translator for Korean. Please join the Discord if interested.</p>
 
 				<div id="funding">
-					<span onClick={this.openFundingLink.bind(this, "streamelements")} ref="streamelements"><img src="https://img.shields.io/badge/Donate-at%20StreamElements-green" alt="Donate at StreamElements" height="20"/></span>
+					<span onClick={this.openFundingLink.bind(this, "paypal")} ref="paypal"><img src="img/buttons/funding/paypal.png" alt="Donate at PayPal" height="20"/></span>
 					<span onClick={this.openFundingLink.bind(this, "kofi")} ref="kofi"><img src="img/buttons/funding/kofi.svg" alt="Donate at Ko-fi" height="20"/></span>
 					<span onClick={this.openFundingLink.bind(this, "patreon")} ref="patreon"><img src="img/buttons/funding/patreon.png" alt="Donate at Patreon" height="20"/></span>
+					<span onClick={this.openFundingLink.bind(this, "streamelements")} ref="streamelements"><img src="https://img.shields.io/badge/Donate-at%20StreamElements-green" alt="Donate at StreamElements" height="20"/></span>
 				</div>
 
 				<h3>Latest Changes</h3>
@@ -41,12 +63,38 @@ class OverlayInfo extends React.Component {
 	}
 
 	openFundingLink(rel) {
-		let search     = window.location.search || "";
-		let search_add = (search.slice(0, 1) === "?") ? "&" : "?";
+		let url = "";
 
-		search += search_add + `default=${rel}`;
+		// Min for PayPal: Hibiya
+		// Min for StreamElements: ngld
+		// Min for Ko-fi: Hibiya
+		// Min for Patreon: ngld
+		switch (this.state.chrome) {
+			case "hibiya":
+			case "actws":
+				switch (rel) {
+					case "paypal":
+					case "kofi":
+						url = DonationService.getRealDonationLink(rel);
 
-		window.open(window.location.pathname + search + "#/settings/donate", "", "width=600,height=430,location=yes,menubar=yes");
+						break;
+
+					default:
+						url = DonationService.buildLocalDonationLink(rel);
+
+						break;
+				}
+
+				break;
+
+			case "ngld":
+			default:
+				url = DonationService.getRealDonationLink(rel);
+
+				break;
+		}
+
+		window.open(url, "", "width=600,height=430,location=yes,menubar=yes");
 	}
 }
 
