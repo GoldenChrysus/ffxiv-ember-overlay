44d307bca08059f82cdf6d2934f5d2da439dd8ba
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2021-04-07 20:59:32 +0900

    Fix new-user default theme

--- src/data/Settings.js
+++ src/data/Settings.js
@@ -21,7 +21,7 @@ const default_settings = {
 		top_right_rank        : false,
 		blur_job_icons        : false,
 		collapse_down         : false,
-		theme                 : "dark",
+		theme                 : "ffxiv-dark",
 		minimal_theme         : false,
 		footer_when_collapsed : false,
 		footer_dps            : false,
